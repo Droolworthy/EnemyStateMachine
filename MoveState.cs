0304b30ad2648d596c2a5b0ac4604ba831d0afbb
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-11-03 13:27:42 +0300

    Update MoveState.cs

--- MoveState.cs
+++ MoveState.cs
@@ -1,51 +1,12 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(Health))]
-public class HealthBar : MonoBehaviour
+public class MoveState : State
 {
-    [SerializeField] private Slider _healthBar;
-    [SerializeField] private Health _health;
+    [SerializeField] private float _speed;
 
-    private Coroutine _coroutine;
-
-    private void OnEnable()
+    private void Update()
     {
-        _health.Changed += OnHealthChanged;
-    }
-
-    private void OnDisable()
-    {
-        _health.Changed -= OnHealthChanged;
-    }
-
-    private void OnHealthChanged(float health)
-    {
-        if (_coroutine != null)
-            StopCoroutine(_coroutine);
-
-        _coroutine = StartCoroutine(TransformWellnessLevel(health));
-    }
-
-    private IEnumerator TransformWellnessLevel(float targetValue)
-    {
-        bool isWork = true;
-
-        int health = 10;
-
-        while (isWork)
-        {
-            _healthBar.value = Mathf.MoveTowards(_healthBar.value, targetValue, Time.deltaTime * health);
-
-            if (_healthBar.value == targetValue)
-            {
-                isWork = false;
-
-                StopCoroutine(_coroutine);
-            }
-
-            yield return null;
-        }
+        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position,
+            _speed * Time.deltaTime);
     }
 }
